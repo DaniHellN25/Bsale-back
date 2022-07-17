ece8111374cdcbf70cedc41f78ddc4d3fb83e103
Pedro Daniel Chi Novelo <danihellnovelo18@gmail.com>
2022-07-16 19:40:45 -0500

    testing heroku headers

--- src/Middlewares/validateHeader.js
+++ src/Middlewares/validateHeader.js
@@ -1,8 +1,9 @@
 //Controlamos que solo las peticiones del cliente lleguen a la base de datos
+//Tambien se permite peticiones por medio de localhost y por el host de herouku
 
 const validateHeader = async (req, res, next) => {
     console.log(req.headers)
-    if(req.headers.origin !== "https://bsale-store-d.vercel.app" || req.headers.origin !== 'https://bsale-store-d.herokuapp.com' ) return res.status(401).send({msg:'Origin Unauthorized'})
+    if(req.headers.origin !== "https://bsale-store-d.vercel.app" && req.headers.host !== 'localhost:3001' && req.headers.host !== 'bsale-store-d.herokuapp.com') return res.status(401).send({msg:'Origin Unauthorized'})
     else return next()
 }
 
